ab2076c43cdf98371500943399823275630b7d4d
Dianapineda20 <dianapineda.dp12@gmail.com>
2020-07-30 15:05:47 -0400

    creadas todas las tablas

--- modelamiento_de_datos/prueba.sql
+++ modelamiento_de_datos/prueba.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE prueba;
+
 CREATE TABLE cliente(
  id SERIAL PRIMARY KEY,
  nombre VARCHAR,
@@ -12,3 +13,25 @@ valor_unitario INT,
 producto VARCHAR,
 descripcion VARCHAR
  );
+
+CREATE TABLE categoria(
+ id_categoria SERIAL PRIMARY KEY,
+ nombre VARCHAR,
+ descripcion VARCHAR
+);
+
+CREATE TABLE factura(
+id_factura SERIAL PRIMARY KEY,
+fecha TIMESTAMP,
+subtotal INT,
+id SERIAL REFERENCES cliente(id)
+);
+
+CREATE TABLE factura_producto(
+id_factura INT,
+id_producto INT,
+cantidad INT,
+FOREIGN KEY (id_factura) REFERENCES factura(id_factura),
+FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
+);
+
